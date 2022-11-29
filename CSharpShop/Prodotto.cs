fe14200044d832c4c7faef34c57123090ac8de33
Pamidonte <francescomarrone8@gmail.com>
2022-11-29 17:38:38 +0100

    compito finito

---
+++ CSharpShop/Prodotto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop
+{
+    public class Prodotto
+    {
+      Random rnd = new Random();
+      private int codice;
+      private string nome;
+      private string descrizione;
+      private int prezzo;
+      private int iva;
+
+        public Prodotto( string nome, string descrizione, int prezzo, int iva)
+        {
+            this.codice = rnd.Next(0,10000000);
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+        public int infoCodice
+        {
+            get { return this.codice; }
+        }
+        public string infoNome
+        {
+            get { return this.nome; }
+            set { this.nome = value; }
+
+        }
+        public string infoDescrizione
+        {
+            get { return this.descrizione; }
+            set { this.descrizione = value; }
+        }
+        public int infoPrezzo
+        {
+            get { return this.prezzo; }
+            set { this.prezzo = value; }
+        }
+        public int infoIva
+        {
+            get { return this.iva; }
+            set { this.iva= value; }
+        }
+        public decimal prezzoIva()
+        {
+            decimal percentualeIva = (this.prezzo * this.iva)/100m;
+            return (this.prezzo + percentualeIva);
+        }
+        public string nomePiuCodice()
+        {
+            return this.codice.ToString() + this.nome;
+        }
+        public string metodoPadLeft()
+        {
+            return this.codice.ToString().PadLeft(8, '0');
+        }
+           
+    }
+}
--- CSharpShop/Program.cs
+++ CSharpShop/Program.cs
@@ -1,2 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 
+namespace CSharpShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            Prodotto primoProdotto = new Prodotto("pizza", "pizza con mozzarella", 1, 20);
+            Prodotto secondoProdotto = new Prodotto("Fanta", "Fanta Orange", 1, 10);
+
+
+
+            Console.WriteLine( "codice prodotto: " + primoProdotto.infoCodice);
+
+            Console.WriteLine("Nome prodotto: " + primoProdotto.infoNome);
+
+            primoProdotto.infoDescrizione = "pizza con ananas";
+            Console.WriteLine("tipo della pizza: " + primoProdotto.infoDescrizione);
+
+            primoProdotto.infoPrezzo = 8;
+            Console.WriteLine("prezzo della pizza: " + primoProdotto.infoPrezzo + " euro");
+
+            primoProdotto.infoIva = 18;
+            Console.WriteLine("l'iva sulla pizza è al: " + primoProdotto.infoIva + "%");
+
+            Console.WriteLine("prezzo con iva: " + primoProdotto.prezzoIva() + " euro");
+
+            Console.WriteLine("codice prodotto con nome : " + primoProdotto.nomePiuCodice());
+
+            Console.WriteLine();    
+
+            
+
+            Console.WriteLine("codice prodotto: " + secondoProdotto.infoCodice);
+
+            Console.WriteLine("Nome prodotto: " + secondoProdotto.infoNome);
+
+            Console.WriteLine("tipo di bevanda: " + secondoProdotto.infoDescrizione);
+
+            Console.WriteLine("prezzo della bevanda: " + secondoProdotto.infoPrezzo + " euro");
+
+            Console.WriteLine("l'iva sulla bevanda è al: " + secondoProdotto.infoIva + "%");
+
+            Console.WriteLine("prezzo con iva: " + secondoProdotto.prezzoIva() + " euro");
+
+            Console.WriteLine("codice prodotto con nome: " + secondoProdotto.nomePiuCodice());
+
+            Console.WriteLine("codice con padLeft: " + secondoProdotto.metodoPadLeft());
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+   
